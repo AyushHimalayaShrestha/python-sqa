b00a0b660cf8909dd1d902119e02438e19d29724
Ayush  Shrestha <aayush625@gmail.com>
2025-09-13 19:50:51 +0545

    armstrong number check in range

---
+++ app/armstrongnumber.py
@@ -0,0 +1,8 @@
+# check armstrong numbers in range
+start = int(input("Enter start: "))
+end = int(input("Enter end: "))
+
+for num in range(start, end + 1):
+    power = len(str(num))
+    if sum(int(d) ** power for d in str(num)) == num:
+        print(num, end=" ")
