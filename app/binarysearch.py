d3a71db3ad3346e1d367a7983b56b222a4d90d9e
Ayush  Shrestha <aayush625@gmail.com>
2025-09-16 21:21:48 +0545

    binary search

---
+++ app/binarysearch.py
@@ -0,0 +1,16 @@
+# Binary Search
+def binary_search(arr, x):
+    low,high = 0, len(arr) -1
+    while low <= high:
+        mid = (low+high) //2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid]<x:
+            low = mid + 1
+        else:
+            high = mid -1
+        return -1
+nums =[10,20,30,40,50,60,70,80,90]
+target= int(input('Enter number to search: '))
+result = binary_search(nums, target)
+print('Found at index' if result != -1 else "Not Found",result)
