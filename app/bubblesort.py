cdc2ca727e84c1ceabacd31db83ac4207fc5d44d
Ayush  Shrestha <aayush625@gmail.com>
2025-09-17 22:10:27 +0545

    bubble sort

---
+++ app/bubblesort.py
@@ -0,0 +1,9 @@
+# Bubble Sort
+nums = [64, 34, 25, 12, 22, 11, 90]
+
+for i in range(len(nums)):
+    for j in range(0, len(nums) - i - 1):
+        if nums[j] > nums[j + 1]:
+            nums[j], nums[j + 1] = nums[j + 1], nums[j]
+
+print("Sorted:", nums)
