9ca82fa7e3bfb17c2f6f3fbe6488c82f6b8c1516
Ayush  Shrestha <aayush625@gmail.com>
2025-10-09 07:07:02 +0545

    custom exception example

---
+++ app/customexecption.py
@@ -0,0 +1,13 @@
+# Custom Execption Example
+class InvalidAgeError(Exception):
+    pass
+
+def check_age(age):
+    if age < 18:
+        raise InvalidAgeError("You must be 18 or older.")
+    print("Access granted!")
+
+try:
+    check_age(15)
+except InvalidAgeError as e:
+    print(e)
