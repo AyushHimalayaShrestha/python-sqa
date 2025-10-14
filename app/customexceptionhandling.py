5d09023ee4252c06eed88e2fe26bcd5e100712bf
Ayush  Shrestha <aayush625@gmail.com>
2025-10-14 21:33:50 +0545

    custom exception handling

---
+++ app/customexceptionhandling.py
@@ -0,0 +1,13 @@
+# Custom execption handling
+class InvalidPasswordError(Exception):
+    pass
+
+def login(password):
+    if len(password) < 6:
+        raise InvalidPasswordError("Password too short!")
+    print("Login successful ✅")
+
+try:
+    login("123")
+except InvalidPasswordError as e:
+    print("❌", e)
