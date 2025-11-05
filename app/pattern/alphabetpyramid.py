207bbfc2302cc9f7a89d30273389362972e58d38
Ayush  Shrestha <aayush625@gmail.com>
2025-11-05 20:42:46 +0545

    alphabet pyramid

---
+++ app/pattern/alphabetpyramid.py
@@ -0,0 +1,9 @@
+# Alphabet Pyramid
+n = 5
+for i in range(1, n + 1):
+    print(" " * (n - i), end="")
+    for j in range(65, 65 + i):
+        print(chr(j), end="")
+    for j in range(65 + i - 2, 64, -1):
+        print(chr(j), end="")
+    print()
