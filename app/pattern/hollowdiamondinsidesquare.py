3983bc2fcc4dffdbe43f4f38dcba121f11c76bf4
Ayush  Shrestha <aayush625@gmail.com>
2025-11-08 08:44:28 +0545

    hollow diamond in square

---
+++ app/pattern/hollowdiamondinsidesquare.py
@@ -0,0 +1,10 @@
+# Hollow Diamond Inside a Square
+n = 7
+for i in range(n):
+    for j in range(n):
+        if (i + j == n // 2) or (j - i == n // 2) or \
+           (i - j == n // 2) or (i + j == n + n // 2 - 1):
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
