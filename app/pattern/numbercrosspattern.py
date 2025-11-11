7b399b313dc4fb0e4bb930a62166f1f91ab87e58
Ayush  Shrestha <aayush625@gmail.com>
2025-11-11 19:28:39 +0530

    number cross pattern

---
+++ app/pattern/numbercrosspattern.py
@@ -0,0 +1,9 @@
+# number cross pattern
+n = 5
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if i == j or j == n - i + 1:
+            print(i, end="")
+        else:
+            print(" ", end="")
+    print()
