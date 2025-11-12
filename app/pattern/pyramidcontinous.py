6a429dae389965fbda73b88f54e36f4f7f638767
Ayush  Shrestha <aayush625@gmail.com>
2025-11-12 07:31:12 +0530

    pyramid with continous alphabets

---
+++ app/pattern/pyramidcontinous.py
@@ -0,0 +1,10 @@
+# Pyramid with continous alphabets
+n = 5
+ch = 65
+for i in range(1, n+1):
+    for j in range(i):
+        print(chr(ch), end=" ")
+        ch += 1
+        if ch > 90:
+            ch = 65
+    print()
